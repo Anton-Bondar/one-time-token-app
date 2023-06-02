37a48caa54cd887201b2ebb93660ac49762cc43d
anbo <AntonBondar2013@gmail.com>
2023-06-02 19:20:38 +0300

    Initial commit

---
+++ src/main/java/org/application/OneTimeTokenApp.java
@@ -0,0 +1,13 @@
+package org.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTimeTokenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneTimeTokenAppApplication.class, args);
+	}
+
+}
